Fix gradient, include c in gradient
- N, delta_c and c are no longer calculated at each nner function evaluation, but only at each outer function evaluation.
- Fixed gradient: added delta_c and c contribution to the gradient, added lower boundary for delta_c to be 10^-6
- The objective function is now (hopefully) continuous, but it is not smooth. Gradient checks fail in cases of parameter values which make simulations collapse very close to each other (because of this 10^-6 fix the gradient is not continuous)

test/test_hard_constraints.py
@@ -49,10 +49,17 @@ def run_optimization(importer, optimizer, history_name, num_starts, min_gap):
                                                                                    'numberofInnerParams': [3, 13, 7, 7, 6, 9, 13, 11],
                                                                                    'deltac': [1, 1, 0.25, 40, 200, 0.3, 0.17, 12]})
     print("Zavrsio sam ovo")
-    nominal=np.asarray([-1.5689175884, -4.9997048936, -2.20969878170002, -1.78600654750001, 4.9901140088, 4.1977354885, 0.693, 0.107])
-    nominal = np.random.uniform(low=-5, high=5, size=8)
-    parameters_grad_check = np.block([nominal, np.random.uniform(low=1, high=1, size=48)])
-    objective.check_grad_multi_eps(parameters_grad_check ).to_csv('grad_check.csv')
+    #nominal=np.asarray([-1.5689175884, -4.9997048936, -2.20969878170002, -1.78600654750001, 4.9901140088, 4.1977354885, 0.693, 0.107])
+    #nominal = np.random.uniform(low=-5, high=5, size=6)
+    nominal = np.block([np.random.uniform(low=-1, high=5, size=1), np.random.uniform(low=-5, high=5, size=1)])
+    print(nominal)
+    #nominal that had bad gradient: IT IS FIXED NOW with the [average - 5e-7, average + 5e+7]
+    #nominal = np.asarray([2.36811251, -2.03154624,  2.61308538,  1.3468213 ,  4.22651353, -2.55029676])
+
+    #another bad nominal
+    nominal = np.asarray([ 1.0054424 , -4.9602817 ,  3.81145327,  3.75943902, -0.63072253, -3.32645144])  
+    parameters_grad_check = np.block([nominal, np.asarray([0.693, 0.107]), np.random.uniform(low=1, high=1, size=48)])
+    objective.check_grad(parameters_grad_check ).to_csv('grad_check.csv')
     breakpoint()
     history_options = pypesto.HistoryOptions(trace_record=True, storage_file=history_name)
     #np.random.seed(num_starts)
@@ -82,7 +89,7 @@ def main():
 
     petab_problem = petab.Problem.from_yaml(
     '/home/zebo/Documents/GitHub/examples/Boehm_JProteomeRes2014OptimalScaling/Boehm_JProteomeRes2014OptimalScaling.yaml')
-    
+        
     #petab_problem = petab.Problem.from_yaml(
     #'/home/zebo/Documents/GitHub/examples/Boehm_JProteomeRes2014OptimalScaling_quantitative/Boehm_JProteomeRes2014OptimalScaling.yaml')
 
@@ -119,7 +126,7 @@ def main():
     optimizer = get_optimizer('L-BFGS-B')
     results = run_optimization(importer, 
                                optimizer, 
-                               history_name =f'histories/Boehm_histories/' + f'test/history_Boehm_' + '_{id}.csv', 
+                               history_name =f'histories/Boehm_histories/' + f'grad_fix_2/history_Boehm_' + '_{id}.csv', 
                                num_starts=200, 
                                min_gap=1e-16)
 
@@ -129,14 +136,14 @@ def main():
                                 scale_y='log10', 
                                 y_limits=2e-17, 
                                 size=(15,6))
-    plt.savefig("plots/Boehm_test_waterfall.png")
+    plt.savefig("plots/Boehm_grad_fix_2_waterfall.png")
 
     pypesto.visualize.parameters([results],
                                  parameter_indices = [0, 1, 2, 3, 4, 5],
                                  size=(15,12), 
                                  legends=['Numerical spline'],
                                  balance_alpha=True)
-    plt.savefig("plots/Boehm_test_parameters.png")
+    plt.savefig("plots/Boehm_grad_fix_2_parameters.png")
 
 
 """